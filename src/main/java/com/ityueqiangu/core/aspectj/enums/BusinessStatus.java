406ef400013c5ab31093280fbae1c80f0a7dfb8f
xiac <xiac@hsit.com.cn>
2022-01-07 10:12:13 +0800

    框架优化完善

---
+++ src/main/java/com/ityueqiangu/core/aspectj/enums/BusinessStatus.java
@@ -0,0 +1,18 @@
+package com.ityueqiangu.core.aspectj.lang.enums;
+
+/**
+ * 操作状态
+ *
+ * @author FlowerStone
+ */
+public enum BusinessStatus {
+    /**
+     * 成功
+     */
+    SUCCESS,
+
+    /**
+     * 失败
+     */
+    FAIL,
+}
---
+++ src/main/java/com/ityueqiangu/core/aspectj/enums/DataSourceType.java
@@ -0,0 +1,18 @@
+package com.ityueqiangu.core.aspectj.lang.enums;
+
+/**
+ * 数据源
+ *
+ * @author FlowerStone
+ */
+public enum DataSourceType {
+    /**
+     * 主库
+     */
+    MASTER,
+
+    /**
+     * 从库
+     */
+    SLAVE
+}
